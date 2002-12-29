ab43c14501c124e2e6be73f910fd576479814aa6
ticso <ticso@6de3e79d-44c3-0310-81b2-c8aaf2c3f825>
2002-12-29 03:48:00 +0000

    fix a syntax error
    git-svn-id: https://devel.bwct.de/svn/projects/libbwct/trunk@232 6de3e79d-44c3-0310-81b2-c8aaf2c3f825

--- bwct/tool.h
+++ bwct/tool.h
@@ -634,7 +634,7 @@ public:
 uint64_t gettimesec(void);
 String sgethostname();
 uint64_t genid();
-String tohex(char *data, int size) {
+String tohex(char *data, int size);
 String genmd5id();
 uint64_t getdate();
 
